721afab57d5142ca2599ffd52efa1f0f90770dc0
Benny Neugebauer <bn@bennyn.de>
2017-08-20 19:00:08 +0200

    test: Testing environment variables on Heroku

--- heroku.js
+++ heroku.js
@@ -3,5 +3,5 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.get('/', (request, response) => response.send('<b>Hello from Heroku!</b>'));
+app.get('/', (request, response) => response.send(`<b>Environment: ${process.env.NODE_ENV}</b>`));
 app.listen(PORT, () => console.log(`Server is running on Heroku.`));
