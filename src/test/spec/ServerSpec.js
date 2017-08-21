b96706860d08e971ebcf853c3dfa8e6d49b60a49
Benny Neugebauer <bn@bennyn.de>
2017-08-21 23:04:35 +0200

    100% code coverage

--- src/test/spec/ServerSpec.js
+++ src/test/spec/ServerSpec.js
@@ -5,25 +5,36 @@ describe('Server', () => {
   let server = undefined;
   const BASE_URL = 'http://localhost:8080/';
 
-  beforeAll((done) => {
-    server = new Server();
-    server.start(done);
-  });
-
-  afterAll((done) => {
-    server && server.stop(done);
-  });
+  afterEach((done) => server && server.stop(done));
 
   describe('"start"', () => {
     it('starts a server with an index page.', (done) => {
-      request.get(BASE_URL, (error, response) => {
-        expect(response.statusCode).toBe(200);
-        done();
+      server = new Server();
+      server.start(() => {
+        request.get(BASE_URL, (error, response) => {
+          expect(response.statusCode).toBe(200);
+          done();
+        });
       });
+    });
+
+    it('executes a callback when a server is running.', (done) => {
+      server = new Server();
+      server.start(done);
+    });
+
+    it('doesn\'t start a server when an instance is already running.', (done) => {
+      server = new Server();
+      server.start(() => server.start(done));
     });
   });
 
   describe('"stop"', () => {
+    beforeEach((done) => {
+      server = new Server();
+      server.start(done);
+    });
+
     it('stops a running server.', (done) => {
       expect(server).toBeDefined();
       expect(server.server).toBeDefined();
