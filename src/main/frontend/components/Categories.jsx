e8a55985f9f1e59effe1e0687c19ced71db5d721
Benny Neugebauer <bn@bennyn.de>
2017-08-30 00:13:52 +0200

    feat: Added Material-UI components

--- src/main/frontend/components/Categories.jsx
+++ src/main/frontend/components/Categories.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import List, {
+  ListItem,
+  ListItemText,
+} from 'material-ui/List';
 
 class Categories extends React.Component {
   renderListItems() {
-    return this.props.categories.map((category) => <li key={category.id}>{category.name}</li>);
+    return this.props.categories.map((category) => {
+      return <ListItem button key={category.id}>
+        <ListItemText primary={category.name} />
+      </ListItem>
+    });
   }
 
   render() {
-    return <ul>{this.renderListItems()}</ul>;
+    return <List dense={false}>{this.renderListItems()}</List>;
   }
 }
 
