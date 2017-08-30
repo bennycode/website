db3c6a9f3f6e982020d68fda4ac6f81f02be2c74
Benny Neugebauer <bn@bennyn.de>
2017-08-31 00:31:50 +0200

    chore(tools): Added Webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 module.exports = {
+  devServer: {
+    contentBase: `${__dirname}/src/main/public`,
+    historyApiFallback: true,
+    port: 8081,
+    proxy: {
+      '/rest': 'http://localhost:8080'
+    }
+  },
   entry: `${__dirname}/src/main/frontend/index.jsx`,
   output: {
     path: `${__dirname}/src/main/public/js`,
+    publicPath: '/js',
     filename: 'index.js'
   },
   module: {
     rules: [
       {
-        test:  /\.(jsx?)$/,
+        test: /\.(jsx?)$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
       }
