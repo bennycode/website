18e2baa423613bce7b125ede94e43e55f93c686f
Benny Neugebauer <bn@bennyn.de>
2017-09-26 18:37:07 +0200

    refactor: Extracted database connection (#21)

--- src/main/route/rest/service/v1/CategoriesRouter.js
+++ src/main/route/rest/service/v1/CategoriesRouter.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const knexfile = require('../../../../../../knexfile');
+const database = require('../../../../connection/database');
 const router = express.Router();
 
-const knex = require('knex')(knexfile[process.env.NODE_ENV]);
-
 function getCategories() {
-  return knex.select().table('categories');
+  return database.select().table('categories');
 }
 
 router.get('/rest/service/v1/categories', (request, response) => {
