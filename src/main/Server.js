57bae47699653c6bafd2cb98da0a4d1cb5ad445c
Benny Neugebauer <bn@bennyn.de>
2017-10-06 15:01:04 +0300

    First working draft

--- src/main/Server.js
+++ src/main/Server.js
@@ -1,5 +1,5 @@
-// const CategoriesRouter = require('./route/rest/service/v1/CategoriesRouter');
 const Hapi = require('hapi');
+const inert = require('inert');
 const path = require('path');
 
 const DEFAULT_CONFIG = {
@@ -9,35 +9,59 @@ const DEFAULT_CONFIG = {
 class Server {
   constructor(config) {
     this.config = Object.assign(DEFAULT_CONFIG, config);
-
-    // this.app = express();
     this.server = undefined;
-
-    this.init();
   }
 
   init() {
-    this.routes();
+    return Promise.resolve().then(() => this.routes());
   }
 
   routes() {
     const PUBLIC_DIR = path.join(__dirname, 'public');
-    // this.app.use(express.static(PUBLIC_DIR));
-    // this.app.use(CategoriesRouter);
+
+    this.server.route({
+      method: 'GET',
+      path: '/{param*}',
+      handler: {
+        directory: {
+          index: true,
+          listing: false,
+          path: PUBLIC_DIR
+        }
+      }
+    });
+
+    this.server.route([
+      require('./route/rest/service/v1/CategoriesRouter')
+    ]);
   }
 
   start(callback) {
     if (this.server) {
       callback();
     } else {
-      // this.server = this.app.listen(this.config.PORT, () => callback(this.config.PORT));
+      this.server = new Hapi.Server();
+      this.server.connection({port: this.config.PORT});
+
+      this.server.register([inert], (error) => {
+        if (error) throw error;
+        this.init().then(() => {
+          this.server.start((error) => {
+            if (error) throw error;
+            callback(this.server.info.port);
+          });
+        });
+      });
     }
   }
 
   stop(callback) {
     if (this.server) {
-      this.server.close(callback);
-      this.server = undefined;
+      this.server.stop({timeout: 1000}, (error) => {
+        if (error) throw error;
+        this.server = undefined;
+        callback();
+      });
     } else {
       callback();
     }
