d523497423b52ab1e49db5744b218a792ecae40f
Benny Neugebauer <bn@bennyn.de>
2017-10-10 16:29:49 +0300

    chore(backend): Server shutdown on "SIGINT" signal

--- heroku.js
+++
@@ -1,4 +0,0 @@
-const Server = require('./src/main/backend/server/Server');
-const server = new Server({PORT: process.env.PORT});
-server.start((port) => console.log(`Server is running on port "${port}".`));
-
