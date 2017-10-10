747e38275781b5531c971b7f6902f2882518a955
Benny Neugebauer <bn@bennyn.de>
2017-10-10 14:39:16 +0300

    Added TestHelper

--- src/test/spec/route/rest/service/v1/CategoriesRouterSpec.js
+++ src/test/spec/route/rest/service/v1/CategoriesRouterSpec.js
@@ -1,47 +1,32 @@
-const request = require('request');
 const CategoriesRouter = require('../../../../../../main/backend/server/route/rest/service/v1/CategoriesRouter');
 const Server = require('../../../../../../main/backend/server/Server');
+const TestHelper = require('../../../../../helpers/TestHelper');
 
 describe('CategoriesRouter', () => {
   let server = undefined;
 
-  const SERVER_PORT = 8072;
-  const BASE_URL = `http://localhost:${SERVER_PORT}`;
-
   beforeAll((done) => {
-    server = new Server({PORT: SERVER_PORT});
+    server = new Server({PORT: TestHelper.SERVER.PORT});
     server.start(() => done());
   });
 
   afterAll((done) => server && server.stop(done));
 
-  function promiseRequest(url) {
-    return new Promise((resolve, reject) => {
-      request.get(url, (error, response) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(response);
-        }
-      });
-    });
-  }
-
   describe('handler', () => {
     it('caches database requests', (done) => {
       spyOn(server.router.categories, 'queryCategories').and.callThrough();
-      const url = `${BASE_URL}${CategoriesRouter.PATH.V1_CATEGORIES}`;
+      const url = `${TestHelper.SERVER.BASE_URL}${CategoriesRouter.PATH.V1_CATEGORIES}`;
 
       Promise.resolve()
-        .then(() => promiseRequest(url))
+        .then(() => TestHelper.promiseRequest(url))
         .then(() => expect(server.router.categories.queryCategories).toHaveBeenCalledTimes(1))
-        .then(() => promiseRequest(url))
+        .then(() => TestHelper.promiseRequest(url))
         .then(() => expect(server.router.categories.queryCategories).toHaveBeenCalledTimes(1))
-        .then(() => promiseRequest(url))
+        .then(() => TestHelper.promiseRequest(url))
         .then(() => expect(server.router.categories.queryCategories).toHaveBeenCalledTimes(1))
         .then(() => {
           server.router.categories.resetCache();
-          return promiseRequest(url);
+          return TestHelper.promiseRequest(url);
         })
         .then(() => expect(server.router.categories.queryCategories).toHaveBeenCalledTimes(2))
         .then(() => done())
--- src/test/spec/server/ServerSpec.js
+++ src/test/spec/server/ServerSpec.js
@@ -1,19 +1,21 @@
 const request = require('request');
 const Server = require('../../../main/backend/server/Server');
+const TestHelper = require('../../helpers/TestHelper');
 
 describe('Server', () => {
   let server = undefined;
 
-  const SERVER_PORT = 8072;
-  const BASE_URL = `http://localhost:${SERVER_PORT}`;
+  beforeEach((done) => {
+    server = new Server({PORT: TestHelper.SERVER.PORT});
+    server.start(done);
+  });
 
   afterEach((done) => server && server.stop(done));
 
   describe('"start"', () => {
     it('starts a server with an index page.', (done) => {
-      server = new Server({PORT: SERVER_PORT});
       server.start(() => {
-        request.get(BASE_URL, (error, response) => {
+        request.get(TestHelper.SERVER.BASE_URL, (error, response) => {
           if (error) return done.fail(error);
           expect(response.statusCode).toBe(200);
           done();
@@ -22,22 +24,15 @@ describe('Server', () => {
     });
 
     it('executes a callback when a server is running.', (done) => {
-      server = new Server({PORT: SERVER_PORT});
       server.start(done);
     });
 
     it('doesn\'t start a server when an instance is already running.', (done) => {
-      server = new Server({PORT: SERVER_PORT});
       server.start(() => server.start(done));
     });
   });
 
   describe('"stop"', () => {
-    beforeEach((done) => {
-      server = new Server({PORT: SERVER_PORT});
-      server.start(done);
-    });
-
     it('stops a running server.', (done) => {
       expect(server).toBeDefined();
       expect(server.server).toBeDefined();
