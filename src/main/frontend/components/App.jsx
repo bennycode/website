c8a9326476b40abaa41dcab940d63a4fee305841
Benny Neugebauer <bn@bennyn.de>
2017-10-10 15:16:25 +0300

    fix(frontend): Fixed spacing between category items

--- src/main/frontend/components/App.jsx
+++ src/main/frontend/components/App.jsx
@@ -23,7 +23,7 @@ class App extends React.Component {
   }
 
   render() {
-    return <Grid container spacing={24}>
+    return <Grid container spacing={16}>
       <Grid item xs={12}>
         <Categories categories={this.state.categories} />
       </Grid>
