02f7f94dfd4d3cd5edb18fe0ea3beaa67a091e19
Panayiotis Lipiridis <lipiridis@gmail.com>
2017-10-10 15:52:32 +0200

    Just prettier

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 require('dotenv').config();
 const Server = require('./src/main/backend/server/Server');
 const server = new Server({PORT: 8080});
-server.start((port) => console.log(`Server is running on port "${port}".`));
+server.start(port => console.log(`Server is running on port "${port}".`));
--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,22 @@
 const development = {
   client: 'sqlite3',
   connection: {
-    filename: './temp/development.db3'
+    filename: './temp/development.db3',
   },
   migrations: {
     tableName: 'knex_migrations',
-    directory: './src/main/backend/database/migrations'
+    directory: './src/main/backend/database/migrations',
   },
   seeds: {
-    directory: './src/test/backend/database/seeds'
+    directory: './src/test/backend/database/seeds',
   },
   useNullAsDefault: true,
 };
 
 const test = Object.assign(development, {
   connection: {
-    filename: './temp/test.db3'
-  }
+    filename: './temp/test.db3',
+  },
 });
 
 const production = {
@@ -24,19 +24,19 @@ const production = {
   connection: `${process.env.DATABASE_URL}?ssl=true`,
   migrations: {
     tableName: 'knex_migrations',
-    directory: './src/main/backend/database/migrations'
+    directory: './src/main/backend/database/migrations',
   },
   pool: {
     min: 2,
-    max: 5
+    max: 5,
   },
   seeds: {
-    directory: './src/main/backend/database/seeds'
+    directory: './src/main/backend/database/seeds',
   },
 };
 
 module.exports = {
   development,
   production,
-  test
+  test,
 };
