b1671b6de9d3b23199384b39eb51ad68370737a4
Benny Neugebauer <bn@bennyn.de>
2017-10-11 14:34:32 +0300

    style(backend): Code formatting with ESLint (#37)

--- src/main/backend/database/migrations/20170912220350_v1.js
+++ src/main/backend/database/migrations/20170912220350_v1.js
@@ -4,12 +4,12 @@ exports.up = (knex, Promise) => {
       table.increments('id').primary().unsigned();
       table.string('color').notNullable();
       table.string('name').notNullable();
-    })
+    }),
   ]);
 };
 
 exports.down = (knex, Promise) => {
   return Promise.all([
-    knex.schema.dropTableIfExists('categories')
+    knex.schema.dropTableIfExists('categories'),
   ]);
 };
