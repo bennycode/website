e602f7902482608fbf81f32c7888563e8047b81e
Benny Neugebauer <bn@bennyn.de>
2017-10-12 15:03:29 +0300

    CommonJS

--- src/main/frontend/utils/FetchUtil.js
+++ src/main/frontend/utils/FetchUtil.js
@@ -1,14 +1,6 @@
-const createReactClass = require('create-react-class');
-
-const FetchUtil = createReactClass({
-  statics: {
-    handleError: function(response) {
-      if (!response.ok) throw new Error(response.statusText);
-      return response;
-    },
+module.exports = {
+  handleError: function(response) {
+    if (!response.ok) throw new Error(response.statusText);
+    return response;
   },
-  render() {
-  },
-});
-
-export default FetchUtil;
+};
