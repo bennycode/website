f8f770128120a2752770d74253b6f2cf96168c15
Benny Neugebauer <bn@bennyn.de>
2017-10-16 20:57:00 +0300

    Action

---
+++ src/main/frontend/modules/status/statusActions.js
@@ -0,0 +1,18 @@
+import FetchUtil from '../../utils/FetchUtil';
+import fetch from 'isomorphic-fetch';
+
+export const FETCHED_VERSION = 'FETCHED_VERSION';
+
+export function fetchVersion() {
+  return dispatch => {
+    return fetch('/status?info=version')
+      .then(FetchUtil.handleError)
+      .then(response => response.json())
+      .then(status => {
+        return {
+          type: FETCHED_VERSION,
+          payload: status.version,
+        };
+      });
+  };
+}
