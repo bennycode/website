40e62402e05a9731cf90fc7caf65bd879f004133
Benny Neugebauer <bn@bennyn.de>
2017-10-16 19:21:12 +0300

    feat: Displaying Server version

--- src/main/frontend/components/App.jsx
+++ src/main/frontend/components/App.jsx
@@ -2,12 +2,14 @@ import Categories from './Categories.jsx';
 import FetchUtil from '../utils/FetchUtil';
 import Grid from 'material-ui/Grid';
 import React from 'react';
+import Typography from 'material-ui/Typography';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       categories: [],
+      version: '',
     };
   }
 
@@ -17,13 +19,29 @@ class App extends React.Component {
       .then(FetchUtil.handleError)
       .then(response => response.json())
       .then(categories => this.setState({...this.state, categories}));
+
+    window
+      .fetch('/status?info=version')
+      .then(FetchUtil.handleError)
+      .then(response => response.json())
+      .then(info => this.setState({...this.state, version: info.version}));
   }
 
   render() {
     return (
       <Grid container={true} spacing={16}>
         <Grid item={true} xs={12}>
+          <Typography type="headline" component="h2">
+            {'Tutorials'}
+          </Typography>
+        </Grid>
+        <Grid item={true} xs={12}>
           <Categories categories={this.state.categories} />
+        </Grid>
+        <Grid item={true} xs={12}>
+          <Typography type="caption" gutterBottom={true} align="left">
+            {`Version ${this.state.version}`}
+          </Typography>
         </Grid>
       </Grid>
     );
