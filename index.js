3f6e7efaf7c5bac56ac61dd032401e2d63807c2f
Benny Neugebauer <bn@bennyn.de>
2017-10-18 23:18:41 +0200

    fix: End process on 'SIGINT'

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const Server = require('./src/main/backend/server/Server');
 const server = new Server({PORT: process.env.PORT});
 
 process.on('SIGINT', () => {
-  server.stop(() => console.log('Stopped server because of "SIGINT" signal.'));
+  server.stop(() => {
+    console.log('Stopped server because of "SIGINT" signal.');
+    process.exit(0);
+  });
 });
 
 server.start((port) => console.log(`Server is running on port "${port}".`));
