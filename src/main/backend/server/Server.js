6ed5736bb01e8fed6ffd09ff6c661700bc4a5cb5
Benny Neugebauer <bn@bennyn.de>
2017-11-05 18:47:45 +0100

    refactor: hapi v17 Migration (#71)

--- src/main/backend/server/Server.js
+++ src/main/backend/server/Server.js
@@ -1,6 +1,7 @@
 const Boom = require('boom');
 const CategoriesRouter = require('./route/rest/service/v1/CategoriesRouter');
 const Hapi = require('hapi');
+const Inert = require('inert');
 const Joi = require('joi');
 const path = require('path');
 
@@ -17,105 +18,99 @@ class Server {
     this.server = undefined;
   }
 
-  init() {
-    this.initRoutes();
-  }
+  getRoutes() {
+    const rootPage = {
+      method: 'GET',
+      path: '/{param*}',
+      options: {
+        handler: {
+          directory: {
+            index: true,
+            listing: false,
+            path: path.join(process.cwd(), 'dist', 'frontend'),
+          },
+        },
+      },
+    };
 
-  initRoutes() {
-    this.server.route([
-      {
-        method: 'GET',
-        path: '/{param*}',
-        config: {
-          handler: {
-            directory: {
-              index: true,
-              listing: false,
-              path: path.join(process.cwd(), 'dist', 'frontend'),
-            },
-          },
+    const categoryPage = {
+      method: 'GET',
+      path: `${CategoriesRouter.PATH.PAGE_CATEGORIES}/{category_slug}`,
+      options: {
+        handler: (request, h) => {
+          return h.file(path.join(process.cwd(), 'dist', 'frontend', 'index.html'));
         },
       },
-      {
-        method: 'GET',
-        path: `${CategoriesRouter.PATH.PAGE_CATEGORIES}/{category_slug}`,
-        config: {
-          handler: (request, reply) => {
-            reply.file(path.join(process.cwd(), 'dist', 'frontend', 'index.html'));
-          },
-        },
-      },
-      {
-        method: 'GET',
-        path: CategoriesRouter.PATH.REST_V1_CATEGORIES,
-        config: {handler: this.router.categories.getCategories},
-      },
-      {
-        method: 'GET',
-        path: `${CategoriesRouter.PATH.REST_V1_CATEGORY}/{category_id}`,
-        config: {
-          handler: this.router.categories.getPlaylistsByCategoryId,
-          validate: {
-            params: {
-              category_id: Joi.number().required(),
-            },
-          },
-        },
-      },
-      {
-        method: 'GET',
-        path: `/status`,
-        config: {
-          handler: (request, reply) => {
-            const {info} = request.query;
+    };
 
-            switch (info) {
-              case 'version':
-                reply({version: this.VERSION});
-                break;
-              default:
-                reply(Boom.notFound(`Supplied parameter value "${info}" is unsupported.`));
-            }
-          },
-          validate: {
-            query: {
-              info: Joi.string()
-                .valid('version')
-                .required(),
-            },
+    const categoryResource = {
+      method: 'GET',
+      path: `${CategoriesRouter.PATH.REST_V1_CATEGORY}/{category_id}`,
+      options: {
+        handler: this.router.categories.getPlaylistsByCategoryId,
+        validate: {
+          params: {
+            category_id: Joi.number().required(),
           },
         },
       },
-    ]);
+    };
+
+    const categoriesResource = {
+      method: 'GET',
+      path: CategoriesRouter.PATH.REST_V1_CATEGORIES,
+      options: {handler: this.router.categories.getCategories},
+    };
+
+    const statusResource = {
+      method: 'GET',
+      path: `/status`,
+      options: {
+        handler: request => {
+          const {info} = request.query;
+          let reply = Boom.notFound(`Supplied parameter value "${info}" is unsupported.`);
+
+          switch (info) {
+            case 'version':
+              reply = {version: this.VERSION};
+              break;
+          }
+
+          return reply;
+        },
+        validate: {
+          query: {
+            info: Joi.string()
+              .valid('version')
+              .required(),
+          },
+        },
+      },
+    };
+
+    return [categoriesResource, categoryPage, categoryResource, rootPage, statusResource];
   }
 
   start(callback) {
     if (this.server) {
       callback();
     } else {
-      this.server = new Hapi.Server();
-      this.server.connection({port: this.config.PORT});
-
-      this.server.register(
-        {
-          register: require('inert'),
-        },
-        error => {
-          if (error) throw error;
-          this.init();
-          this.server.start(error => {
-            if (error) throw error;
-            callback(this.server.info.port);
-          });
-        }
-      );
+      this.server = Hapi.Server({port: this.config.PORT});
+      this.server
+        .register([
+          {
+            plugin: Inert,
+          },
+        ])
+        .then(() => this.server.route(this.getRoutes()))
+        .then(() => this.server.start())
+        .then(() => callback(this.server.info.port));
     }
   }
 
   stop(callback) {
     if (this.server) {
-      this.server.stop({timeout: 1000}, error => {
-        if (error) throw error;
+      this.server.stop().then(() => {
         this.server = undefined;
         callback();
       });
