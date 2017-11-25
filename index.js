e92556f1564283ebb82e6f4f707ed33580fe4c44
Benny Neugebauer <bn@bennyn.de>
2017-11-25 15:54:49 +0100

    chore: React specific linting rules for ESLint

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+const logger = require('logdown')('index');
 const Server = require('./src/main/backend/server/Server');
 const server = new Server({PORT: process.env.PORT});
 
 process.on('SIGINT', () => {
   server.stop(() => {
-    console.log('Stopped server because of "SIGINT" signal.');
+    logger.warn('Stopped server because of "SIGINT" signal.');
     process.exit(0);
   });
 });
 
-server.start((port) => console.log(`Server is running on port "${port}".`));
+server.start((port) => logger.info(`Server is running on port "${port}".`));
