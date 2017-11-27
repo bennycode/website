e8a12b2430bf7fee10d12ee48378e514166b1533
Benny Neugebauer <bn@bennyn.de>
2017-11-27 18:35:29 +0100

    mocking store

--- src/main/frontend/components/CategoryList.test.jsx
+++ src/main/frontend/components/CategoryList.test.jsx
@@ -1,8 +1,38 @@
 import CategoryList from './CategoryList';
+import {Provider} from 'react-redux';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const withStore = (children, store) => {
+  return <Provider store={store}>{children}</Provider>;
+};
+
+const mockStore = () => configureStore([thunk.withExtraArgument()]);
+
+
 test('works', () => {
   expect(true).toBe(true);
-  renderer.create(<CategoryList />);
+  const state = {
+    categoryState: {
+      categories: [{
+        color: "#8CBE29",
+        id: 7,
+        name: "Android",
+        slug: "android",
+      }],
+    },
+    statusState: {
+      version: '0.0.7',
+    },
+  };
+
+  const component = renderer.create(
+    withStore(<CategoryList />, mockStore()(state),
+    ),
+  );
+
+  expect(true).toBe(true);
 });
