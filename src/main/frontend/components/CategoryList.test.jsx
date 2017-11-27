aac1336c4cd84d99eeb31fd133dbe9fd5a0c925e
Benny Neugebauer <bn@bennyn.de>
2017-11-27 16:21:55 +0100

    test: Testing React front-end with Jest

---
+++ src/main/frontend/components/CategoryList.test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import CategoryList from './CategoryList';
+
+test('works', () => {
+  expect(true).toBe(true);
+  // renderer.render(<TutorialList />);
+});
